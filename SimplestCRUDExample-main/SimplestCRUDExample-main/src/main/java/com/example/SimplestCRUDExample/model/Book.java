8dee93a911205f5a0183a9393ef6e8f616e13774
EarnestMD <85481264+EarnestMD@users.noreply.github.com>
2024-06-26 22:46:10 +0530

    Add files via upload

---
+++ SimplestCRUDExample-main/SimplestCRUDExample-main/src/main/java/com/example/SimplestCRUDExample/model/Book.java
@@ -0,0 +1,44 @@
+package com.example.SimplestCRUDExample.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="Books")
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column
+    private String title;
+
+    @Column
+    private String author;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+
+
+}
